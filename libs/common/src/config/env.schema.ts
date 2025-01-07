84f091c18b7d2bd94be34591841014691884a648
Emad Abbasnezhad <real_emad_abbasnezhad@proton.me>
2025-01-07 20:19:10 +0330

    init prisma

--- libs/common/src/config/env.schema.ts
+++ libs/common/src/config/env.schema.ts
@@ -1,5 +1,6 @@
 import Joi from "joi";
 
 export const ENV_SCHEMA =  Joi.object({
-    PORT: Joi.number().default(3000)
+    PORT: Joi.number().default(3000),
+    PG_URL: Joi.string().required()
 } as Joi.SchemaMap);

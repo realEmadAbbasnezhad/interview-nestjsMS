7c7119f73b9fb27ad1ed68445170d7c71f93556f
Emad Abbasnezhad <real_emad_abbasnezhad@proton.me>
2025-01-07 20:40:54 +0330

    update ports env

--- libs/common/src/config/env.schema.ts
+++ libs/common/src/config/env.schema.ts
@@ -1,6 +1,8 @@
 import Joi from "joi";
 
 export const ENV_SCHEMA =  Joi.object({
-    PORT: Joi.number().default(3000),
+    PORT_GATEWAY: Joi.number().default(3000),
+    PORT_GIFTCODE: Joi.number().default(3001),
+    PORT_LOGGER: Joi.number().default(3002),
     PG_URL: Joi.string().required()
 } as Joi.SchemaMap);

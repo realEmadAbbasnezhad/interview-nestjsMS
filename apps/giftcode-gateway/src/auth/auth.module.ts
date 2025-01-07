9184840134dd5533d2b4dcd99b11d8a9102f22b1
Emad Abbasnezhad <real_emad_abbasnezhad@proton.me>
2025-01-07 22:46:23 +0330

    delete repository from common and move it under auth

--- apps/giftcode-gateway/src/auth/auth.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import {RepositoryModule} from "common/common/repository/repository.module";
-
-@Module({
-    imports:[RepositoryModule]
-})
-export class AuthModule {}
--- libs/common/src/repository/repository.module.ts
+++
@@ -1,9 +0,0 @@
-import {Module} from '@nestjs/common';
-import {PrismaService} from "common/common/repository/prisma/prisma.service";
-
-@Module({
-    providers: [PrismaService],
-    exports: [PrismaService],
-})
-export class RepositoryModule {
-}
